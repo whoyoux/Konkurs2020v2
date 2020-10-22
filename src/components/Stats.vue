cb3d39e1f5b8969750a797b19db53faa5937db69
whoyoux <48690153+whoyoux@users.noreply.github.com>
2020-10-22 12:44:35 +0200

    Change Stats.vue

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -1,40 +1,41 @@
 <template>
 <b-container fluid>
-    <b-row class="row" v-if="withAll">
-        <b-col md="12" lg="6" class="col mb-3" v-if="withAll">
-            <div class="info">
-                <h4 style="color: var(--warning)">{{parseInt(all).toLocaleString()}}</h4>
-                <h5 style="color: #a0aec0">{{allText}}</h5>
-            </div>
-        </b-col>
-        <b-col md="12" lg="6" class="col mb-3" v-if="withAll">
-            <div class="info">
-                <h4 style="color: var(--danger)">{{parseInt(allDeaths).toLocaleString()}}</h4>
-                <h5 style="color: #a0aec0">{{allDeathsText}}</h5>
-            </div>
-        </b-col>
+    <b-row class="mb-3" v-if="withAll">
+      <b-col col cols="12" md="6">
+        <b-card class="p-3 text-center" no-body>
+          <h4 style="color: var(--warning)">{{parseInt(all).toLocaleString()}}</h4>
+          <h5 style="color: #a0aec0">{{allText}}</h5>
+        </b-card>
+      </b-col>
+      <b-col col cols="12" md="6">
+        <b-card class="p-3 text-center" no-body>
+          <h4 style="color: var(--warning)">{{parseInt(allDeaths).toLocaleString()}}</h4>
+          <h5 style="color: #a0aec0">{{allDeathsText}}</h5>
+        </b-card>
+      </b-col>
     </b-row>
-    <b-row class="row">
-        <b-col md="12" lg="4" class="col">
-            <div class="info">
-                <h4 style="color: var(--warning)">{{parseInt(sick).toLocaleString()}}</h4>
-                <h5 style="color: #a0aec0">{{sickText}}</h5>
-            </div>
-        </b-col>
-        <b-col md="12" lg="4" class="col">
-            <div class="info">
-                <h4 style="color: var(--success)">{{parseInt(recovery).toLocaleString()}}</h4>
-                <h5 style="color: #a0aec0">{{recoveryText}}</h5>
-            </div>
-        </b-col>
-        <b-col md="12" lg="4" class="col">
-            <div class="info">
-                <h4 style="color: var(--danger)">{{parseInt(deads).toLocaleString()}}</h4>
-                <h5 style="color: #a0aec0">{{deadsText}}</h5>
-            </div>
-        </b-col>
+    <b-row>
+      <b-col cols="12" md="4">
+        <b-card class="p-3 text-center" no-body>
+          <h4 class="text-warning">{{parseInt(sick).toLocaleString()}}</h4>
+          <h5 style="color: #a0aec0">{{sickText}}</h5>
+        </b-card>
+      </b-col>
+      <b-col cols="12" md="4">
+        <b-card class="p-3 text-center" no-body>
+          <h4 class="text-success">{{parseInt(recovery).toLocaleString()}}</h4>
+          <h5 style="color: #a0aec0">{{recoveryText}}</h5>
+        </b-card>
+      </b-col>
+      <b-col cols="12" md="4">
+        <b-card class="p-3 text-center" no-body>
+          <h4 class="text-danger">{{parseInt(deads).toLocaleString()}}</h4>
+          <h5 style="color: #a0aec0">{{deadsText}}</h5>
+        </b-card>
+      </b-col>
     </b-row>
-</b-container>
+  </b-container>
+
 </template>
 
 <script>
@@ -62,10 +63,6 @@ export default {
     margin-right: auto;
 }
 
-.col {
-    height: 100px;
-    display: table;
-}
 
 @media (max-width: 992px) {
     .col {
@@ -79,11 +76,4 @@ export default {
     }
 }
 
-.info {
-    border: 1px solid #bfc9d6;
-    border-radius: 4px;
-    display: table-cell;
-    vertical-align: middle;
-    text-align: center;
-}
 </style>
