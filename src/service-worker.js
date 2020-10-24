f0ac994e4c2ace5f07b3ac2c2d68d66706090bc8
whoyoux <48690153+whoyoux@users.noreply.github.com>
2020-10-24 21:09:56 +0200

    Added service worker

---
+++ src/service-worker.js
@@ -0,0 +1,12 @@
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+workbox.core.setCacheNameDetails({prefix: "KoronawirusAPP"});
+
+self.addEventListener('message', (event) => {
+
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
