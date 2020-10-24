e198df3b310eb7be499e803aaa13f44685525f43
whoyoux <48690153+whoyoux@users.noreply.github.com>
2020-10-24 21:25:00 +0200

    Update service-worker.js

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,6 +2,13 @@ self.addEventListener('install', () => {
     self.skipWaiting();
 })
 
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+   });
 self.addEventListener('message', (event) => {
 
   if (event.data && event.data.type === 'SKIP_WAITING') {
